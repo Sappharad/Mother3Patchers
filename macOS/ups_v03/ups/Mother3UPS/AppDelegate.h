85955f7860db392f778b9e88be13480bf663a456
Sappharad <Sappharad@users.noreply.github.com>
2020-11-23 14:44:55 -0600

    Apple Silicon support
    Also, close the app when you close the window

---
+++ macOS/ups_v03/ups/Mother3UPS/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Mother3UPS
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ macOS/ups_v03/ups/Mother3UPS/AppDelegate.m
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.m
+//  Mother3UPS
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+-(BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender{
+    return YES;
+}
+
+@end
